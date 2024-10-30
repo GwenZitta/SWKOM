ef6412cc60ecfbcfa062eb94385e8e07fb384c81
Yusrasfef <yusrasfef.007@gmail.com>
2024-10-30 22:25:02 +0100

    1,2,3,Sprint _4

---
+++ GGY/src/main/java/com/example/ggy/service/RabbitMQSender.java
@@ -0,0 +1,22 @@
+package main.java.com.example.ggy.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ggy.config.RabbitMQConfig;
+
+@Service
+public class RabbitMQSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMQSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "document.routingKey", message);
+    }
+}
